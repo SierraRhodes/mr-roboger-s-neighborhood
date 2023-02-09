2b498cf625eddacc1f4b08390e3b375f17a33faa
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-09 15:53:05 -0800

    Test adding boop to 2 passed

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,33 @@ function beepBoop(num) {
   let numbers = [];
   for (let i = 0; i <= num; i++) {
     if (targets.includes(i)) {
-      numbers.push("beep"); 
+      numbers.push("Beep!"); 
     } else { 
       numbers.push(i);
     }
   }
   return numbers;
+}
+
+
+
+
+function beepBoop(num) {
+  const targets = [1];
+  const targets2 = [2];
+  let numbers = [];
+  for (let i = 0; i <= num; i++) {
+    if (targets.includes(i)) {
+      numbers.push("Beep!"); 
+    } else { 
+      numbers.push(i);
+    }
+   if (targets2.includes(i)) {
+      numbers.push("Boop!");
+  } else {
+    numbers.push(i);
+  }
+
+  return numbers;
+}
 }
