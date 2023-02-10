515acd0ab2ee01740f9b4b58a1afe5cb91beabfc
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-09 16:40:09 -0800

    Test including more numbers that correlate to 1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -20,24 +20,57 @@ function beepBoop(num) {
 }
 
 
-
-
 function beepBoop(num) {
   const targets = [1];
   const targets2 = [2];
   let numbers = [];
   for (let i = 0; i <= num; i++) {
     if (targets.includes(i)) {
-      numbers.push("Beep!"); 
-    } else { 
+      numbers.push("Beep!");
+    } else if (targets2.includes(i)) {
+      numbers.push("Boop!");
+    } else {
       numbers.push(i);
     }
-   if (targets2.includes(i)) {
-      numbers.push("Boop!");
-  } else {
-    numbers.push(i);
   }
-
   return numbers;
 }
+
+function beepBoop(num) {
+  const targets = [1];
+  const targets2 = [2];
+  const targets3 = [3];
+  let numbers = [];
+  for (let i = 0; i <= num; i++) {
+    if (targets.includes(i)) {
+      numbers.push("Beep!");
+    } else if (targets2.includes(i)) {
+      numbers.push("Boop!");
+    } else if (targets3.includes(i)) {
+      numbers.push("Won't you be my neighbor?");
+    } else {
+      numbers.push(i);
+    }
+  }
+  return numbers;
+}
+
+function beepBoop(num) {
+  const targets = [1];
+  const targets2 = [2];
+  const targets3 = [3];
+  let numbers = [];
+  for (let i = 0; i <= num; i++) {
+    let iString = i.toString();
+    if (targets.some(target =>iString.includes(target))) {
+      numbers.push("Beep!");
+    } else if (targets2.some(target =>iString.includes(target))) {
+      numbers.push("Boop!");
+    } else if (targets3.some(target => iString.includes(target))) {
+      numbers.push("Won't you be my neighbor?");
+    } else {
+      numbers.push(i);
+    }
+  }
+  return numbers;
 }
